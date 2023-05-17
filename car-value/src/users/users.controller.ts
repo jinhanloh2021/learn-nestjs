5463bf77ecf4fa30934f775eedadb1ff36086338
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-05-18 01:14:48 +0800

    Add some controllers for get and delete.

--- car-value/src/users/users.controller.ts
+++ car-value/src/users/users.controller.ts
@@ -1,12 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+  HttpCode,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.userService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string /* Id retrieved as string */) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  @HttpCode(204)
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
   }
 }
